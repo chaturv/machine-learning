bd23f0d95249d0bb54859f6a7ed82cb6f7e4db3e
chaturv <chaturv@users.noreply.github.com>
2016-05-29 01:19:28 +0800

    scatter3
    scatter3

--- week9/machine-learning-ex8/ex8/yahoo.m
+++ week9/machine-learning-ex8/ex8/yahoo.m
@@ -5,14 +5,22 @@ data = csvread('./yahoo_data/GOOG_20140101-20160528.csv', 1, 5);
 prc = data(:,2);
 vol = data(:,1);
 
+% vol = zeros(606,1);
+% for i=1:605
+%     vol(i, 1) = 1000000;
+% end   
+% vol(606, 1) = 2000000;
+
+%plot(vol)
+
 %sort
-% prc = sort(prc);
-% vol = sort(vol);
+prc = sort(prc);
+vol = sort(vol);
 
 mu_prc = mean(prc);
 sigma_prc = std(prc);
 
-% normalize
+% normalize - price
 prc = (prc - mu_prc) ./ sigma_prc;
 mu_prc = mean(prc);
 sigma_prc = std(prc);
@@ -26,7 +34,7 @@ pdf_prc = pdf(pd_prc, prc);
 mu_vol = mean(vol);
 sigma_vol = std(vol);
 
-% normalize
+% normalize - volume
 vol = (vol - mu_vol) ./ sigma_vol;
 mu_vol = mean(vol);
 sigma_vol = std(vol);
@@ -37,16 +45,27 @@ pdf_vol = pdf(pd_vol, vol);
 % plot(vol, pdf_vol);
 
 mu = [mu_prc mu_vol];
-Sigma = [sigma_prc .5; .5 sigma_vol]; 
+Sigma = [sigma_prc 0; 0 sigma_vol]; 
 
 % ceate axis
 prc_x = linspace(min(prc), max(prc), 50);
 vol_y = linspace(min(vol), max(vol), 50);
+ 
+% prc_x = prc;
+% vol_y = vol;
+
 
 [PRC, VOL] = meshgrid(prc_x, vol_y);
 F = mvnpdf([PRC(:) VOL(:)], mu, Sigma);
 F = reshape(F, length(vol_y), length(prc_x));
 
-surf(prc_x, vol_y, F);
+hold on;
+grid on;
 axis auto;
+
+surf(prc_x, vol_y, F);
+scatter3(0, 4, 0.01, 'red', 'LineWidth', 2);
+
 xlabel('Price (normalized)'); ylabel('Vol (normalized)'); zlabel('Probability Density');
+
+hold off;
